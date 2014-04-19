6bd01d59e8628c78211e6eb0138c913920d3be06
Hexcles Ma <bob1211@gmail.com>
2014-04-19 19:38:32 +0800

    add power_mod to demo and force __DEBUG__ in Makefile

--- Makefile
+++ Makefile
@@ -3,11 +3,12 @@ CFLAGS=-std=c++11 -O2 -Wall
 
 all: bigint.o bigint_demo
 
-bigint.o:
+bigint.o: bigint.cpp bigint.h
 	$(CC) bigint.cpp -o bigint.o -c $(CFLAGS)
 
-bigint_demo:
-	$(CC) bigint.o bigint_demo.cpp -o bigint_demo $(CFLAGS)
+# Do not reuse bigint.o so that __DEBUG__ macro will work
+bigint_demo: bigint_demo.cpp bigint.h bigint.cpp
+	$(CC) bigint.cpp bigint_demo.cpp -o bigint_demo $(CFLAGS) -D__DEBUG__
 
 .PHONY: clean
 clean:
--- bigint_demo.cpp
+++ bigint_demo.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 int main() {
-    BigInt a, b;
+    BigInt a, b, c;
     cout << "Please input operand a:";
     cin >> a;
     cout << "Please input operand b:";
     cin >> b;
+    cout << "Please input operand c:";
+    cin >> c;
     cout << "a + b = " << a + b << endl;
     cout << "a - b = " << a - b << endl;
     cout << "a * b = " << a * b << endl;
     cout << "a / b = " << a / b << endl;
     cout << "a \% b = " << a % b << endl;
-    cout << "a ^ b = " << power(a, b) << endl;
+    if ( b < 100000 ) cout << "a ^ b = " << power(a, b) << endl;
+    cout << "a ^ b (mod c) = " << power(a, b, c) << endl;
     return 0;
 }
